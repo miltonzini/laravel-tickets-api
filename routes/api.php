af8ed8829eedc0cdfb695a2a8e5e6378f428f1af
Milton Zini <miltonzini@gmail.com>
2026-04-25 14:21:49 -0300

    Add Ticket model, migration, factory and seed

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,13 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Models\Ticket;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
+
+// temp 
+Route::get('/tickets', function(){
+    return Ticket::paginate(20);
+});
