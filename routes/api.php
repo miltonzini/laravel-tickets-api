97a57e2e84791ca924ecf4bed691b7b9c2532a57
Milton Zini <miltonzini@gmail.com>
2026-04-25 14:35:39 -0300

    Add v1 prefix to API routes

--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,13 @@ use App\Http\Controllers\AuthController;
 use App\Models\Ticket;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/register', [AuthController::class, 'register']);
 
-// temp 
-Route::get('/tickets', function(){
-    return Ticket::paginate(20);
+Route::prefix('v1')->group(function(){
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
+    
+    // temp 
+    Route::get('/tickets', function(){
+        return Ticket::paginate(20);
+    });
 });
